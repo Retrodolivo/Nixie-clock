101c8ff7772235786fee8563e03f0fe1668ddbf7
Retroboy <naskin.iv@gmail.com>
2021-03-28 00:13:17 +0300

    refactor "i2c"

---
+++ Software/_Nixie_clock/i2c.c
@@ -0,0 +1,47 @@
+#include "i2c.h"
+
+
+void i2c_init(enum i2c_speed_t speed)
+{
+	switch(speed)
+	{
+		case Standart:
+			TWBR = (F_CPU / 100000 - 16) / 2;
+			break;
+			
+		case Fast:
+			TWBR = (F_CPU / 400000 - 16) / 2;
+			break;
+	}
+}
+
+void i2c_start_condition(void)
+{
+	/*Set HIGH at SCL(TWINT), Generate START(TWSTA), Enable I2C(TWEN)*/
+	TWCR = 1<<TWINT | 1<<TWSTA | 1<<TWEN;
+	/*Wait till SCL LOW*/
+	while(!(TWCR & 1<<TWINT));			 
+}
+
+void i2c_stop_condition(void)
+{
+	/*Clear interrupt flag(TWINT), Generate STOP(TWSTO)*/
+	TWCR = 1<<TWINT | 1<<TWSTO | 1<<TWEN;
+}
+
+void i2c_sendbyte(uint8_t byte)
+{
+	TWDR = byte;
+	TWCR = 1<<TWINT | 1<<TWEN;
+	/*Wait till SCL LOW*/
+	while(!(TWCR & 1<<TWINT));
+}
+
+uint8_t i2c_readbyte(void)
+{
+	/*Set HIGH at SCL(TWINT), Enable I2C(TWEN), Set ACK bit(TWEA)*/
+	TWCR = 1<<TWINT | 1<<TWEN | 1<<TWEA;
+	/*Wait till SCL LOW*/
+	while(!(TWCR&(1<<TWINT)));
+	return TWDR;
+}
---
+++ Software/_Nixie_clock/i2c.h
@@ -0,0 +1,14 @@
+#ifndef I2C_H_
+#define I2C_H_
+
+#include "main.h"
+
+enum i2c_speed_t {Standart, Fast};
+
+void i2c_init(enum i2c_speed_t speed);
+void i2c_start_condition(void);
+void i2c_stop_condition(void);
+void i2c_sendbyte(uint8_t byte);
+uint8_t i2c_readbyte(void);
+
+#endif /* I2C_H_ */
